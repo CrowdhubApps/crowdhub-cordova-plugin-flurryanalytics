2776d3b44f3778b6f48eed8849c46736bad535da
Austin Isakson <austinjisakson@gmail.com>
2023-08-02 19:17:29 +0200

    force kotlin in android project, version bump

--- www/FlurryAnalytics.d.ts
+++ www/FlurryAnalytics.d.ts
@@ -1,3 +1,19 @@
 declare module 'crowdhub-cordova-plugin-flurryanalytics/www/FlurryAnalyticsPlugin';
 
-export interface FlurryAnalyticsPlugin {}
+export enum LogLevel {
+	VERBOSE = 'VERBOSE',
+	DEBUG = 'DEBUG',
+	INFO = 'INFO',
+	WARN = 'WARN',
+	ERROR = 'ERROR'
+}
+
+export interface FlurryAnalyticsPlugin {
+	initialize(
+		apiKey: string,
+		logLevel?: LogLevel,
+		crashReportingEnabled?: boolean,
+		appVersion?: string,
+		iapReportingEnabled?: boolean
+	): void;
+}
